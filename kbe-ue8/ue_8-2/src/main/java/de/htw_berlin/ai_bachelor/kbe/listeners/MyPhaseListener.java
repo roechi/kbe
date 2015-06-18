65b5370ad64a3af0e10cf3f0414f3d1fb401f280
Roechi <msg.remus@gmail.com>
2015-06-18 15:35:12 +0200

    Aufgabe c.) erledigt. Sollte aber noch mal gecheckt werden.

--- kbe-ue8/ue_8-2/src/main/java/de/htw_berlin/ai_bachelor/kbe/listeners/MyPhaseListener.java
+++ kbe-ue8/ue_8-2/src/main/java/de/htw_berlin/ai_bachelor/kbe/listeners/MyPhaseListener.java
@@ -12,11 +12,18 @@ public class MyPhaseListener implements PhaseListener {
 	private static final long serialVersionUID = 1L;
 
 	public void afterPhase(PhaseEvent arg0) {
-		System.out.println("Exiting Phase: " + arg0.getPhaseId().toString());
+		if (arg0.getPhaseId().getName().equals(PhaseId.RESTORE_VIEW.getName()) || 
+				arg0.getPhaseId().getName().equals(PhaseId.RENDER_RESPONSE.getName())) {
+			if (arg0.getPhaseId().getName().equals(PhaseId.RESTORE_VIEW.getName()))
+				System.out.println("Is Postack Request: " + arg0.getFacesContext().isPostback());
+			System.out.println("ID of component tree root: " + arg0.getFacesContext().getViewRoot().getId());
+			System.out.println("Number of nodes in view component tree: " + arg0.getFacesContext().getViewRoot().getChildCount());
+		} else
+			System.out.println("Exiting Phase: " + arg0.getPhaseId().getName().toString());
 	}
 
 	public void beforePhase(PhaseEvent arg0) {
-		System.out.println("Entering Phase: " + arg0.getPhaseId().toString());
+		System.out.println("Entering Phase: " + arg0.getPhaseId().getName().toString());
 	}
 
 	public PhaseId getPhaseId() {
