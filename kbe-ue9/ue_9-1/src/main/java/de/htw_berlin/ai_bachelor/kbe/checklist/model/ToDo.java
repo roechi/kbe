329463377555c94ca1e10f56e61b351887292a5a
Roechi <msg.remus@gmail.com>
2015-06-26 13:08:53 +0200

    Aufgabe 9-1 a.) fertig.

--- kbe-ue9/ue_9-1/src/main/java/de/htw_berlin/ai_bachelor/kbe/checklist/model/ToDo.java
+++ kbe-ue9/ue_9-1/src/main/java/de/htw_berlin/ai_bachelor/kbe/checklist/model/ToDo.java
@@ -1,6 +1,9 @@
 package de.htw_berlin.ai_bachelor.kbe.checklist.model;
 
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class ToDo implements Serializable {
 	
@@ -8,15 +11,25 @@ public class ToDo implements Serializable {
 
 	private String name;
 	private boolean done = false;
+	private Date date;
 	
-	private ToDo(String name, boolean done) {
+	private ToDo(String name, boolean done, Date date) {
 		super();
 		this.name = name;
 		this.done = done;
+		this.date = date;
+	}
+	
+	public ToDo(String name, Date date) {
+		this(name, false, date);
 	}
 	
 	public ToDo(String name) {
-		this(name,false);
+		this(name,false, new Date());
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.DAY_OF_YEAR, 1);
+		Date tomorrow = calendar.getTime();
+		setDate(tomorrow);
 	}
 	
 	public boolean isDone() {
@@ -30,6 +43,13 @@ public class ToDo implements Serializable {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public Date getDate() {
+		return date;
 	}
 
 }
