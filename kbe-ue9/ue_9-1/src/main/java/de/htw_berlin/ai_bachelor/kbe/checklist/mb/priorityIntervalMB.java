3e977374326d0e0dd52e8d0368c95b85e4c6e043
Roechi <msg.remus@gmail.com>
2015-07-09 13:18:59 +0200

    EditPriorityInterval view angelegt.

--- kbe-ue9/ue_9-1/src/main/java/de/htw_berlin/ai_bachelor/kbe/checklist/mb/priorityIntervalMB.java
+++ kbe-ue9/ue_9-1/src/main/java/de/htw_berlin/ai_bachelor/kbe/checklist/mb/priorityIntervalMB.java
@@ -2,19 +2,22 @@ package de.htw_berlin.ai_bachelor.kbe.checklist.mb;
 
 import java.io.Serializable;
 
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.validation.constraints.Min;
 
-@ManagedBean(name="list", eager=true)
-@SessionScoped
+@ManagedBean(name="priority", eager=true)
+@ApplicationScoped
 public class priorityIntervalMB implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private final int min = 1;
+	@Min(value=1)
+	private int min;
 	private int max;
     
 	public priorityIntervalMB() {
+		this.min = 1;
 		this.max = 2;
 	}    
 
@@ -24,6 +27,22 @@ public class priorityIntervalMB implements Serializable {
 
     public String save() {
     	return "save";
+    }
+    
+    public int getMin() {
+    	return min;
+    }
+    
+    public void setMin(int min) {
+    	this.min = min;
+    }
+    
+    public int getMax() {
+    	return max;
+    }
+    
+    public void setMax(int max) {
+    	this.max = max;
     }
     
 }
