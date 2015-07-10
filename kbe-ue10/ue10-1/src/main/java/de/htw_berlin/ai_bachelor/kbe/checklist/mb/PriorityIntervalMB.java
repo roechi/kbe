ef711abcd89d5d1282d6812740f5dc983e776a76
Roechi <msg.remus@gmail.com>
2015-07-10 12:48:50 +0200

    Uebung 10 angefangen.

---
+++ kbe-ue10/ue10-1/src/main/java/de/htw_berlin/ai_bachelor/kbe/checklist/mb/PriorityIntervalMB.java
@@ -0,0 +1,44 @@
+package de.htw_berlin.ai_bachelor.kbe.checklist.mb;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.validation.constraints.Min;
+
+@ManagedBean(name="priority", eager=true)
+@ApplicationScoped
+public class PriorityIntervalMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	
+	private final int min = 1;
+	@Min(value=2)
+	private int max;
+    
+	public PriorityIntervalMB() {
+		this.max = 2;
+	}    
+
+	public PriorityIntervalMB(int max) {
+		this.max = max;
+	}	
+
+    public String save() {
+    	return "save";
+    }
+    
+    public int getMin() {
+    	return min;
+    }
+    
+    public int getMax() {
+    	return max;
+    }
+    
+    public void setMax(int max) {
+    	this.max = max;
+    }
+    
+}
---
+++ kbe-ue10/ue10-1/src/main/java/de/htw_berlin/ai_bachelor/kbe/checklist/model/ToDo.java
@@ -0,0 +1,72 @@
+package de.htw_berlin.ai_bachelor.kbe.checklist.model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class ToDo implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	
+	private boolean done = false;
+	
+	@Future(message="Das Fälligkeitsdatum muss in der Zukunft liegen.")
+	private Date date;
+	
+	@MyInterval
+	private int priority;
+	
+	private ToDo(String name, boolean done, Date date) {
+		this.name = name;
+		this.done = done;
+		this.date = date;
+		this.priority = 1;
+	}
+	
+	public ToDo(String name, Date date) {
+		this(name, false, date);
+	}
+	
+	public ToDo(String name) {
+		this(name,false, new Date());
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.DAY_OF_YEAR, 1);
+		Date tomorrow = calendar.getTime();
+		setDate(tomorrow);
+	}
+	
+	public boolean isDone() {
+		return done;
+	}
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public int getPriority() {
+		return priority;
+	}
+	
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	
+}
